62fe963f8f2596d088dd8786e9696c7805f31bad
daviziih <davianjogabriel@gmail.com>
2022-01-27 15:30:18 -0300

    by daviziih

--- main.js
+++ main.js
@@ -25,3 +25,30 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image, 
+  #about .text, #about .image, 
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links`,
+  {
+    interval: 100
+  }
+)
